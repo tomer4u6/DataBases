a085b4729d32a8863d734d4c313412e2cdb34c23
tomer4u6 <tb9541@bs.amalnet.k12.il>
2018-12-25 16:35:08 +0200

    Version 1003

--- app/src/main/java/com/example/tomer/databases/MainActivity.java
+++ app/src/main/java/com/example/tomer/databases/MainActivity.java
@@ -1,10 +1,13 @@
-//Version 1002
+//Version 1003
 package com.example.tomer.databases;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -30,6 +33,25 @@ public class MainActivity extends AppCompatActivity {
         hlp = new HelperDB(this);
         db = hlp.getWritableDatabase();
         db.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String st = item.getTitle().toString();
+        if(st.equals("Credits")){
+            Toast.makeText(this, "This app was created by Tomer Ben Ari", Toast.LENGTH_SHORT).show();
+        }
+        if (st.equals("Change Activity")){
+            Intent t = new Intent(this, SecondActivity.class);
+            startActivity(t);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void getDataStudent(View view) {
--- app/src/main/java/com/example/tomer/databases/SecondActivity.java
+++ app/src/main/java/com/example/tomer/databases/SecondActivity.java
@@ -1,9 +1,13 @@
 package com.example.tomer.databases;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
     ListView lv;
@@ -16,5 +20,24 @@ public class SecondActivity extends AppCompatActivity {
 
         lv = (ListView)findViewById(R.id.lv);
         spr = (Spinner)findViewById(R.id.spr);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String st = item.getTitle().toString();
+        if(st.equals("Credits")){
+            Toast.makeText(this, "This app was created by Tomer Ben Ari", Toast.LENGTH_SHORT).show();
+        }
+        if (st.equals("Change Activity")){
+            Intent t = new Intent(this, MainActivity.class);
+            startActivity(t);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
